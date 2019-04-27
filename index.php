5e6d004faa41605f59b9b473b1a5ba6d4d0e239d
jusus88 <jesussmithcentenoaraujo@gmail.com>
2019-04-26 19:33:41 -0500

    mi primer bloque

--- index.php
+++ index.php
@@ -2,10 +2,35 @@
 	$cantidad=0;
 	$nombres="smith"
 	// es un  lenguaje de expresiones
-	if ($nombres)
-	echo "Este mensaje es si la expresion devuelve true"; 
-	echo "<br/>linea2: Este mensaje es si la expre devuelve true" ;
-	//
+	//echo "Este mensaje es si la expresion devuelve true"; 
+	//echo "<br/>linea2: Este mensaje es si la expre devuelve true" ;
+	
+ ?>	
+ <table>
+ 	<tr>
+ 		<th>nombres</th>
+ 		<th>Apellidos</th>
+ 		<th>DNI</th>
+ 	</tr>
+ 	<?php if (1): ?>
+ 		<style type="text/css">
+ 			td {
+ 				background: green;
+ 				color: white;
+ 			}
+ 		</style>
+ 		<tr>
+ 			<td> Luis</td>
+ 			<td> gutierrez</td>
+ 			<td> 12465478</td>
+ 		</tr>
+ 		<tr>
+ 			<td> juan</td>
+ 			<td>huaroto</td>
+ 			<td> 15645664</td>
+ 		</tr>
+ 		<?php endid; ?>
+ 		
+ </table>
 
 
- ?>
