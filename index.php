49d2bda58503422fe55b0f23f537fd94565721e2
jusus88 <jesussmithcentenoaraujo@gmail.com>
2019-04-26 19:13:44 -0500

    mi extructura

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+	$cantidad=0;
+	$nombres="smith"
+	// es un  lenguaje de expresiones
+	if ($nombres)
+	echo "Este mensaje es si la expresion devuelve true"; 
+	echo "<br/>linea2: Este mensaje es si la expre devuelve true" ;
+	//
+
+
+ ?>
